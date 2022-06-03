c8fe2a41f6723e0314e4761a9fbbe9d48289ce56
Happiness-James <gesarehjames@gmail.com>
2022-06-03 07:55:12 +0300

    signup commit

--- app/src/main/java/com/happinessjames/workoutlog/LoginActivity.kt
+++ app/src/main/java/com/happinessjames/workoutlog/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -13,20 +14,28 @@ class LoginActivity : AppCompatActivity() {
     lateinit var etEmail:TextInputEditText
     lateinit var tilPassword:TextInputLayout
     lateinit var etPassword:TextInputEditText
+    lateinit var tvSignup:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        btnLogin = findViewById(R.id.btnLogin)
-        tilEmail = findViewById(R.id.tilEmail)
-        etEmail = findViewById(R.id.etEmail)
-        tilPassword = findViewById(R.id.tilPassword)
-        etPassword = findViewById(R.id.etPassword)
+        clickView()
+
+        tvSignup.setOnClickListener { val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent) }
 
         btnLogin.setOnClickListener {
             validateLogin()
         }
 
 
+    }
+    fun clickView(){
+        btnLogin = findViewById(R.id.btnLogin)
+        tilEmail = findViewById(R.id.tilEmail)
+        etEmail = findViewById(R.id.etEmail)
+        tilPassword = findViewById(R.id.tilPassword)
+        etPassword = findViewById(R.id.etPassword)
+        tvSignup = findViewById(R.id.tvSignup)
     }
     fun validateLogin(){
         var error = false
---
+++ app/src/main/java/com/happinessjames/workoutlog/SignupActivity.kt
@@ -0,0 +1,84 @@
+package com.happinessjames.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class SignupActivity : AppCompatActivity() {
+    lateinit var btnSignup:Button
+    lateinit var tilFirstname:TextInputLayout
+    lateinit var etFirstname: TextInputEditText
+    lateinit var tilLastname:TextInputLayout
+    lateinit var etLastname:TextInputEditText
+    lateinit var tilEmailsignup:TextInputLayout
+    lateinit var etEmailsignup:TextInputEditText
+    lateinit var tilPasswordsignup: TextInputLayout
+    lateinit var etPasswordsignup: TextInputEditText
+    lateinit var tilConfirmation:TextInputLayout
+    lateinit var etConfirmation:TextInputEditText
+    lateinit var tvLogin:TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+        clickViewSignup()
+
+        btnSignup.setOnClickListener {
+            validationSignup()
+        }
+        tvLogin.setOnClickListener { val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent) }
+    }
+    fun clickViewSignup(){
+        btnSignup = findViewById(R.id.btnSignup)
+        tilFirstname = findViewById(R.id.tilFirstname)
+        etFirstname = findViewById(R.id.etFirstname)
+        tilLastname = findViewById(R.id.tilLastname)
+        etLastname = findViewById(R.id.etLastname)
+        tilEmailsignup = findViewById(R.id.tilEmailsignup)
+        etEmailsignup = findViewById(R.id.etEmailsignup)
+        tilPasswordsignup = findViewById(R.id.tilPasswordsignup)
+        etPasswordsignup = findViewById(R.id.etPasswordsignup)
+        tilConfirmation = findViewById(R.id.tilConfirmation)
+        etConfirmation = findViewById(R.id.etConfirmation)
+        tvLogin = findViewById(R.id.tvLogin)
+
+    }
+    fun validationSignup(){
+        var error = false
+        tilFirstname.error = null
+        tilLastname.error = null
+        tilEmailsignup.error = null
+        tilPasswordsignup.error = null
+        tilConfirmation.error = null
+        var firstname = etFirstname.text.toString()
+        if (firstname.isBlank()){
+            tilFirstname.error = " First name is required"
+            error = true
+        }
+        var lastname = etLastname.text.toString()
+        if (lastname.isBlank()){
+            tilLastname.error = " Last name is required"
+            error = true
+        }
+        var emailsignup = etEmailsignup.text.toString()
+        if (emailsignup.isBlank()){
+            tilEmailsignup.error = " Email is required"
+            error = true
+        }
+        var passwordsignup = etPasswordsignup.text.toString()
+        if (passwordsignup.isBlank()){
+            tilPasswordsignup.error = " Password is required"
+            error = true
+        }
+        var confirmation = etConfirmation.text.toString()
+        if (confirmation.isBlank()){
+            tilConfirmation.error = " Confirm Password is required"
+            error = true
+        }
+    }
+}
